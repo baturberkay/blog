ababc2ad31d9d60f97e19c46fb0780b4204e44fc
baturberkay <baturberkay@gmail.com>
2020-02-08 00:05:07 +0300

    Updates

---
+++ precache-manifest.bbf02db30e22af1a17a05de95dd2f4f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221e21d54a3c68bfe037c2ee2f79ba60",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "1d079beee81ebfaa699d",
+    "url": "/blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa6263a454a9e1db146a",
+    "url": "/blog/static/js/2.703a70f2.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/blog/static/js/2.703a70f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d079beee81ebfaa699d",
+    "url": "/blog/static/js/main.6524430e.chunk.js"
+  },
+  {
+    "revision": "cbfd563c8600f0c401a0",
+    "url": "/blog/static/js/runtime-main.3e812e36.js"
+  }
+]);
